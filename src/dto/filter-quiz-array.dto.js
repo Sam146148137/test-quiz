38708c8c76aa8b997e6ce28493522ad8811fae92
sam <zaqaryan.sam@gmail.com>
2022-09-12 17:50:17 +0400

    added image field in get my profile query for member

--- src/dto/filter-quiz-array.dto.js
+++ src/dto/filter-quiz-array.dto.js
@@ -1,3 +1,5 @@
+import config from '../config/variables.config';
+
 class FilterQuizDto {
   static filterQuizArray(quizArray) {
     const array = [];
@@ -6,7 +8,7 @@ class FilterQuizDto {
         quizId: item.quizId,
         score: item.score,
         title: item.quiz[0].title,
-        image: item.quiz[0].image
+        image: `${config.PROTOCOL}://${config.HOST}/uploadedImage/${item.quiz[0].image}`
       });
     });
     return array;
