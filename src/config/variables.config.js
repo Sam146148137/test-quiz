80c4dcadd7fef45b09d94e25bd620377aea2d91e
Narek <nr.petrosyan96@gmail.com>
2022-09-12 13:54:31 +0400

    added role.admin for my profile

--- src/config/variables.config.js
+++ src/config/variables.config.js
@@ -7,8 +7,8 @@ const config = {
   UPLOAD_IMAGES: 'uploadImage',
   MONGODB: {
     // eslint-disable-next-line max-len
-    // URL: process.env.MONGODB_URL || 'mongodb+srv://test_quiz_admin:test_quiz_admin@cluster0.qmy3kch.mongodb.net/?retryWrites=true&w=majority'
-    URL: 'mongodb://localhost:27017/test_quiz'
+    URL: process.env.MONGODB_URL || 'mongodb+srv://test_quiz_admin:test_quiz_admin@cluster0.qmy3kch.mongodb.net/?retryWrites=true&w=majority'
+    // URL: 'mongodb://localhost:27017/test_quiz'
   },
   // PROTOCOL: 'http',
   PROTOCOL: 'https',
