c80c3a84200b318cf62459bae9a4a969aa58e076
sam <zaqaryan.sam@gmail.com>
2022-09-15 20:27:24 +0400

    cors

--- src/config/variables.config.js
+++ src/config/variables.config.js
@@ -3,8 +3,7 @@ const config = {
   PORT: process.env.PORT || 3040,
   ONE_WAY_HASH_SECRET: process.env.ONE_WAY_HASH_SECRET,
   DISABLE_REQUEST_LOG: process.env.DISABLE_REQUEST_LOG,
-  // CORS: process.env.CORS?.split(',') ,
-  CORS: 'https://national-assembly-app.herokuapp.com/signup',
+  CORS: process.env.CORS?.split(',') || '*',
   UPLOAD_IMAGES: 'uploadedImage',
   MONGODB: {
     URL: process.env.MONGODB_URL || 'mongodb+srv://test_quiz_admin:test_quiz_admin@cluster0.qmy3kch.mongodb.net/Quiz'
@@ -14,7 +13,7 @@ const config = {
   AUTH: {
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
-    ACCESS_TOKEN_ACTIVE_TIME: process.env.ACCESS_TOKEN_ACTIVE_TIME || '1m',
+    ACCESS_TOKEN_ACTIVE_TIME: process.env.ACCESS_TOKEN_ACTIVE_TIME || '1h',
     REFRESH_TOKEN_ACTIVE_TIME: process.env.REFRESH_TOKEN_ACTIVE_TIME || '12h'
   },
 
