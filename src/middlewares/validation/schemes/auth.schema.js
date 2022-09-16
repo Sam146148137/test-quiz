48d53a987d960ac85cfd36269bb2cc654bd1dc58
sam <zaqaryan.sam@gmail.com>
2022-09-16 20:04:27 +0400

    firstname lastname length fix firstname max length is 255 symbols, lastname max length is 255 symbols, password max length is 255 symbols

--- src/middlewares/validation/schemes/auth.schema.js
+++ src/middlewares/validation/schemes/auth.schema.js
@@ -4,7 +4,7 @@ import Joi from 'joi';
 const AuthSchema = {
   loginSchema: {
     body: Joi.object({
-      password: Joi.string().min(7).required(),
+      password: Joi.string().min(7).max(255).required(),
       email: Joi.string().email().required()
     })
   }
